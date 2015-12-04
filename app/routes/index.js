16869a4c7220f131dc25ee04d8fb9edc35da1aee
Gerlando Piro <gerlando@gmail.com>
2015-12-04 11:30:35 -0800

    Allow setting of zip codes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return $.getJSON(`http://api.wunderground.com/api/13fcb02d16148708/conditions/forecast/q/CA/San_Francisco.json`);
+  model(params) {
+    let zipcode = params.zipcode || '97212';
+    return $.getJSON(`http://api.wunderground.com/api/13fcb02d16148708/conditions/forecast/q/${zipcode}.json`);
   },
 
-  afterModel: function(model, transition) {
-    model.forecast.days = model.forecast.simpleforecast.forecastday.slice(1,4)
+  afterModel: function(model) {
+    model.zipcode = model.current_observation.display_location.zip;
+    model.forecast.days = model.forecast.simpleforecast.forecastday.slice(1,4);
   },
 
   actions: {
-    updateZip() {
-      console.log('Just Do it!');
+    updateZip(model) {
+      this.transitionTo('index', model.zipcode);
     }
   }
 });
